bb92eb3c4e47fbb15279781348c6ccf09b3599f8
arswarog <arswarog@yandex.ru>
2018-03-01 10:16:38 +0300

    setColor

--- snake.js
+++ snake.js
@@ -1,43 +1,81 @@
-let game={
-	pole:[],
-	snake:[],
-	width:20,
-	height:20,
-	init: function(){
-		for (x=0; x<this.width; x++) {
-			this.pole[x]=[];
-			for (y=0; y<this.height; y++){
-				this.pole[x][y]=0;
-			}
-		}
-		this.snake[0]={x:1,y:2};
-	},
-	// создаем поле
-	table: function(){
-		document.write("<table>");
-		for (i=0;i<this.width;i++){
-			document.write("<tr>")
-			for (k=0;k<this.height;k++){
-				document.write(`<td id="${i}-${k}"></td>`);
-			}
-			document.write("</tr>");
-		}
-		document.write("</table>");
-	},
-	// отображение тела змейки
-	view: function(){
-		for (i=0;i<20;i++){
-			for (k=0;k<20;k++){
-				if (this.pole[i][k]){
-					$(`#${i}-${k}`).css("background-color","green");
-				}
-			}
-		}
-		for (let i of this.snake){
-			$(`#${i.x}-${i.y}`).css("background-color","black");
-			console.log(i);
-		}
-	}
+let game = {
+    pole  : [],
+    snake : [],
+    width : 20,
+    height: 20,
+
+    init: function () {
+        for (let x = 0; x < this.width; x++) {
+            this.pole[x] = [];
+            for (let y = 0; y < this.height; y++) {
+                this.pole[x][y] = 0;
+            }
+        }
+
+        // получаем случайные координаты для головы змеи
+        // но делаем отступ от края на 2 ячейки
+        // что бы она сразу не врезалась в стену
+        let x         = Math.floor(Math.random() * (this.width - 4)) + 2;
+        let y         = Math.floor(Math.random() * (this.height - 4)) + 2;
+        this.snake[0] = {x: x, y: y};
+        switch (Math.floor(Math.random() * 4)) {
+            case 0:
+                this.snake[1] = {x: x - 1, y: y};
+                break;
+            case 1:
+                this.snake[1] = {x: x + 1, y: y};
+                break;
+            case 2:
+                this.snake[1] = {x: x, y: y + 1};
+                break;
+            case 3:
+                this.snake[1] = {x: x, y: y - 1};
+                break;
+        }
+    },
+
+    // создаем поле
+    table: function () {
+        document.write('<table>');
+        for (let x = 0; x < this.width; x++) {
+            document.write('<tr>')
+            for (let y = 0; y < this.height; y++) {
+                document.write(`<td id="${x}-${y}"></td>`);
+            }
+            document.write('</tr>');
+        }
+        document.write('</table>');
+    },
+
+    // функция для задания цвета отдельной ячейки
+    setColor: function (x, y, color) {
+        $(`#${x}-${y}`).css('background-color', color);
+    },
+
+    // отображение тела змейки
+    view: function () {
+        // перерисовываем поле
+        for (x = 0; x < this.width; x++) {
+            for (y = 0; y < this.height; y++) {
+                let color = '#f5f5f5';
+                if (this.pole[x][y])
+                    color = '#097054';
+                this.setColor(x, y, color);
+            }
+        }
+
+        // обрабатываем змейку
+        for (let i in this.snake) {
+            let item = this.snake[i];
+            console.log(i, typeof i)
+            if (i > 0)
+            // Тело змейки
+                this.setColor(item.x, item.y, '#ff9900');
+            else
+            // Голова змейки
+                this.setColor(item.x, item.y, '#6599ff');
+        }
+    }
 };
 game.init();
 game.table();
